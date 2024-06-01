9709252c42dcba869eea2c17095d9a8c8268cd7d
Victor <btf.victorsf0206@gmail.com>
2024-06-01 13:25:01 -0300

    primeiro desafio resolvido

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,10 +1,28 @@
 // CAC-TAT.spec.js created with Cypress
 /// <reference types="Cypress" />
 
-describe('Central de Atendimento ao Cliente TAT', function() {
+const { delay } = require("cypress/types/bluebird")
+
+
+    describe('Central de Atendimento ao Cliente TAT', function(){
+        beforeEach(function(){
+            cy.visit('./src/index.html')
+    })
     it('verifica o título da aplicação', function() 
     {
-        cy.visit('./src/index.html')
         cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
     })
+
+    it('preenche os campos obrigatórios e envia o formulário', function() 
+    {
+        cy.get('#firstName').type('Victor')
+        cy.get('#lastName').type('Souza')
+        cy.get('#email').type('victorsouza@gmail.com')
+        cy.get('#open-text-area').type('Um teste automatizado')
+ //Sintaxe é estruturada em ('button[(nome da propriedade)= tipo da propriedade])
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success').should('be.visible')
+    })
   })
+    
