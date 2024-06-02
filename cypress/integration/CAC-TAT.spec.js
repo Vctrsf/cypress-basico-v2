bdb9357de4bbdd9453f270eb0d04b0c65e5d1763
Victor <btf.victorsf0206@gmail.com>
2024-06-01 21:16:26 -0300

    Segundo exercicio extra resolvido

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,9 +1,6 @@
 // CAC-TAT.spec.js created with Cypress
 /// <reference types="Cypress" />
 
-const { delay } = require("cypress/types/bluebird")
-
-
     describe('Central de Atendimento ao Cliente TAT', function(){
         beforeEach(function(){
             cy.visit('./src/index.html')
@@ -15,14 +12,38 @@ const { delay } = require("cypress/types/bluebird")
 
     it('preenche os campos obrigatórios e envia o formulário', function() 
     {
+        //const tempoDeDigitaçao = : também é possível criar uma variável para utilizar como delay
+        //wait é uma função que determina o tempo entre um teste .type e outro
+        //const longText = 'Textao,Textao,Textao,Textao,Textao,Textao,Textao,'
+        
         cy.get('#firstName').type('Victor')
+
         cy.get('#lastName').type('Souza')
+
         cy.get('#email').type('victorsouza@gmail.com')
-        cy.get('#open-text-area').type('Um teste automatizado')
+
+        cy.get('#open-text-area').type('Um texto automatizado')
+
  //Sintaxe é estruturada em ('button[(nome da propriedade)= tipo da propriedade])
         cy.get('button[type="submit"]').click()
 
         cy.get('.success').should('be.visible')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+        
+        cy.get('#firstName').type('Victor')
+
+        cy.get('#lastName').type('Souza')
+
+        cy.get('#email').type('victorsouza')
+
+        cy.get('#open-text-area').type('Um texto automatizado')
+
+ //Sintaxe é estruturada em ('button[(nome da propriedade)= tipo da propriedade])
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.error').should('be.visible')
     })
   })
     
